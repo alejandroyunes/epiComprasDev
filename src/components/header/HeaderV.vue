f2594f7373d81237d18a5065aced7b675f7a5df0
Alejandro Cano <alejandro.cano@imagineapps.co>
2024-03-10 15:04:20 -0500

    added design system

--- src/components/header/HeaderV.vue
+++ src/components/header/HeaderV.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import './header.scss'
-// import InputV from '@/components/header/input/InputV.vue'
-
+import InputV from '@/components/header/input/InputV.vue'
 
 </script>
 
@@ -10,14 +9,24 @@ import './header.scss'
     <nav class="inner">
 
       <div class="nav-one">
-        <img alt="" class="logo" src="@/assets/logo.svg" width="125" height="45" />
-        <!-- <InputV /> -->
-        <div>city</div>
+        <img alt="" class="logo" src="@/assets/logo.svg" width="35" height="30" />
+        <InputV />
       </div>
 
       <div class="nav-two">
-        <div>icon</div>
-        <button>enter</button>
+
+        <div class="left">
+          <div class="logo-mobile">
+            <img alt="" class="logo" src="@/assets/logo.svg" width="30" height="28" />
+          </div>
+          <div>city</div>
+        </div>
+
+        <div class="right">
+          <div>icon</div>
+          <button>enter</button>
+        </div>
+
       </div>
 
     </nav>
--- src/components/header/input/InputV.vue
+++ src/components/header/input/InputV.vue
@@ -1,56 +1,15 @@
 <script setup lang="ts">
-// import SearchSvg from '@/assets/icons/SearchSvg.vue'
-// import ArrowRightSvg from '../../../../public/icons/ArrowRightSvg.vue'
+import './input.scss'
+import SearchSvg from '../../icons/SearchSvg.vue'
+import ArrowRightSvg from '../../icons/ArrowRightSvg.vue'
 </script>
 
 <template>
 
-  <div class="main">
-    <div class="icon">
-      <!-- <SearchSvg /> -->
-    </div>
-    <form class="form">
-      <input class="input" type="text" placeholder="Buscar" />
-    </form>
-    <div class="arrow">
-      <!-- <ArrowRightSvg /> -->
-    </div>
-  </div>
+  <form class="form">
+    <SearchSvg class="search" />
+    <input class="input" type="text" placeholder="Buscar" />
+    <ArrowRightSvg class="arrow" />
+  </form>
+
 </template>
-
-<style lang="scss">
-.main {
-  display: "flex";
-  position: "relative";
-}
-
-.icon {
-  position: "absolute";
-  top: "50%";
-  left: 8;
-  transform: "translateY(-50%)";
-}
-
-.form {
-  width: "100%";
-}
-
-.input {
-  border: 1px solid red;
-  border-radius: 16px;
-  padding: 8px 16px;
-
-  :focus {
-    border-color: red
-  }
-}
-
-.arrow {
-  cursor: "pointer";
-  position: "absolute";
-  top: "50%";
-  right: 8;
-  transform: "translateY(-50%)";
-}
-</style>
-
