db30f9b4130385d90ad9a7db2299897c2029a4b3
Alejandro Cano <alejandroyunes@outlook.com>
2024-03-16 23:24:33 -0500

    remove public from path

--- src/components/categories/categoriesData.ts
+++ src/components/categories/categoriesData.ts
@@ -1,58 +1,58 @@
 export const categories = [
   {
     name: 'Carros y Motos',
-    image: '/public/categories/carros.webp'
+    image: '/categories/carros.webp'
   },
   {
     name: 'Propiedades y Alquileres',
-    image: '/public/categories/inmobilario.webp'
+    image: '/categories/inmobilario.webp'
   },
   {
     name: 'Teléfonos Inteligentes',
-    image: '/public/categories/smartphones.webp'
+    image: '/categories/smartphones.webp'
   },
   {
     name: 'Artículos del Hogar',
-    image: '/public/categories/home.webp'
+    image: '/categories/home.webp'
   },
   {
     name: 'Portátiles',
-    image: '/public/categories/laptop.webp'
+    image: '/categories/laptop.webp'
   },
   {
     name: 'Deportes',
-    image: '/public/categories/futbol.webp'
+    image: '/categories/futbol.webp'
   },
   {
     name: 'Mascotas',
-    image: '/public/categories/pets.webp'
+    image: '/categories/pets.webp'
   },
   {
     name: 'Instrumentos',
-    image: '/public/categories/guitar.webp'
+    image: '/categories/guitar.webp'
   },
   {
     name: 'Servicios',
-    image: '/public/categories/services.webp'
+    image: '/categories/services.webp'
   },
   {
     name: 'Arte',
-    image: '/public/categories/art.webp'
+    image: '/categories/art.webp'
   },
   {
     name: 'Moda y Belleza',
-    image: '/public/categories/vestido.webp'
+    image: '/categories/vestido.webp'
   },
   {
     name: 'Herramientas',
-    image: '/public/categories/tools.webp'
+    image: '/categories/tools.webp'
   },
   {
     name: 'Otros',
-    image: '/public/categories/other.webp'
+    image: '/categories/other.webp'
   },
   {
     name: 'Electrodomésticos',
-    image: '/public/categories/homeappliances.webp'
+    image: '/categories/homeappliances.webp'
   }
 ]
