3bdecc6ab9fc4c70f5e90a9569da1ad4b026cb1a
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-21 10:22:51 -0500

    remove class

--- src/components/ads/AdsC.vue
+++ src/components/ads/AdsC.vue
@@ -7,7 +7,7 @@ import TitleC from '@/components/title/TitleC.vue'
 <template>
   <TitleC :view="'ver más'" :title="'Anuncios Recientes'" :border="false" />
 
-  <div class="ads-outer">
+  <div>
     <div v-for="ad in ads" :key="ad.id" class="item">
       <p>{{ ad.date }}</p>
       <p>{{ ad.description }}</p>
