491067c5ecb1e20614daf8957aa46cd9538df3a4
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-21 09:53:18 -0500

    fou

--- src/components/ads/AdsC.vue
+++ src/components/ads/AdsC.vue
@@ -8,34 +8,12 @@ import TitleC from '@/components/title/TitleC.vue'
 <template>
   <TitleC :view="'ver más'" :title="'Anuncios Recientes'" :border="false" />
 
-
-  <div class="ads-item">
-    <div class="item">Item 1</div>
-    <div class="item">Item 2</div>
-    <div class="item">Item 3</div>
-    <div class="item">Item 4</div>
+  <div class="ads-inner">
+    <div v-for="ad in ads" :key="ad.id" class="item">
+      <p>{{ ad.date }}</p>
+      <p>{{ ad.description }}</p>
+    </div>
   </div>
-
-
-  <div class="ads-item">
-    <ul>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-      <li>Item 4</li>
-    </ul>
-  </div>
-
-  <div class="ads-item">
-    <ul>
-      <li v-for="ad in ads" :key="ad.id">
-        <p>{{ ad.date }}</p>
-        <p>{{ ad.description }}</p>
-      </li>
-    </ul>
-  </div>
-
-
 
 
 </template>
