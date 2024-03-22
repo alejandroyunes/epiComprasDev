84437440d38789ec9b9691074d2541a488a1534d
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-21 23:55:44 -0500

    clean class f

--- src/components/ads/AdsList.vue
+++ src/components/ads/AdsList.vue
@@ -5,11 +5,23 @@ import { ads } from './data'
 </script>
 
 <template>
-  <section class="testing-class-one">
+  <section class="ads-container">
     <ul>
-      <li v-for="category in ads" :key="category.id" class="item">
-        <p>{{ category.description }}</p>
-        <p>hello</p>
+      <li v-for="ad in ads" :key="ad.id" class="item">
+        <RouterLink :to="ad.url">
+          <img class="ad-image" :src="ad.image" alt="" width="300px" height="165px">
+        </RouterLink> 
+        <div class="ad-info">
+          <div class="ad-title">
+            <h3>{{ ad.title }}</h3>
+            <HeartSvg class="ad-heart" />
+            <HeartFillSvg v-show="false" class="ad-heart-fill" />
+          </div>
+          <p>{{ ad.description }}</p>
+          <p>{{ ad.price }}</p>
+          <p>{{ ad.location }}</p>
+          <p>{{ ad.date }}</p>
+        </div>
       </li>
     </ul>
 
