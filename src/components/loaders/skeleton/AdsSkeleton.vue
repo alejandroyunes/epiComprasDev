0e78ac217e43175cdff17a54663fcdf38b9f8f5e
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-23 17:43:00 -0500

    final touches scorll snap type

--- src/components/loaders/skeleton/AdsSkeleton.vue
+++ src/components/loaders/skeleton/AdsSkeleton.vue
@@ -6,17 +6,23 @@ import './ads-skeleton.scss'
   <section class="ads-skeleton">
     <ul>
       <li v-for="index in 5" :key="index">
-        <div class="shine-line card-img">
-          <span class="circle"></span>
-        </div>
-        <div class="ads-info-inner">
-          <p class="shine-line card-title"></p>
-          <p class="shine-line card-description-long"></p>
-          <p class="shine-line card-description-medium"></p>
-          <p class="shine-line card-description-short"></p>
 
-          <p class="shine-line card-description-shorter margin-top-24 "></p>
-          <p class="shine-line card-description-shorter"></p>
+        <div class="li-container">
+
+          <div class="li-item">
+            <div class="shine-line card-img">
+              <span class="circle"></span>
+            </div>
+            <div class="ads-info-inner">
+              <p class="shine-line card-title"></p>
+              <p class="shine-line card-description-long"></p>
+              <p class="shine-line card-description-medium"></p>
+              <p class="shine-line card-description-short"></p>
+
+              <p class="shine-line card-description-shorter margin-top-24 "></p>
+              <p class="shine-line card-description-shorter"></p>
+            </div>
+          </div>
         </div>
       </li>
     </ul>
