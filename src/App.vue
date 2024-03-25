67f72ee80760e847db2a9f1b68d8061eae1bf0de
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-24 19:21:07 -0500

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import HeaderC from '@/components/header/HeaderC.vue'
+import Footer from '@/components/footer/FooterC.vue'
 
 </script>
 
 <template>
-  <HeaderC />
+  <HeaderC /> 
   <RouterView />
+  <Footer />
 </template>
---
+++ src/components/footer/FooterC.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import './footer.scss'
+</script>
+
+<template>
+  <footer class="footer-outer">
+    <div class="footer-inner">
+      <p>© 2021 EpiCompras</p>
+    </div>
+  </footer>
+</template>
