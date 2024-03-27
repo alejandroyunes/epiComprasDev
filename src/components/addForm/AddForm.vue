9e01a2b963cdaf5cbed1814b567d5e395378c48a
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-27 02:05:50 -0500

    adding post icons styles

--- src/components/addForm/AddForm.vue
+++ src/components/addForm/AddForm.vue
@@ -11,25 +11,25 @@ import ServicesSvg from '@/components/icons/ServicesSvg.vue'
     <h1 class="add-form-title">¡Hola! Cuéntanos, ¿qué vas a publicar?</h1>
     <div class="add-form-services">
 
-      <div>
+      <div class="add-service-item">
         <p>Inmuebles</p>
         <div class="add-service-svg">
           <HouseServiceSvg />
         </div>
       </div>
-      <div>
+      <div class="add-service-item">
         <p>Productos</p>
         <div class="add-service-svg">
           <ServicesSvg />
         </div>
       </div>
-      <div>
+      <div class="add-service-item">
         <p>Vehículos</p>
         <div class="add-service-svg">
           <CarServiceSvg />
         </div>
       </div>
-      <div>
+      <div class="add-service-item">
         <p>Servicios</p>
         <div class="add-service-svg">
           <GamePadSvg />
