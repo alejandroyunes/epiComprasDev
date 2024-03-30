0e911a13f166766000db12bd53f9b2ab692780e6
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-30 17:37:10 -0500

    added real estate icons

--- src/components/ad-post/rent-or-sale/RentOrSell.vue
+++ src/components/ad-post/rent-or-sale/RentOrSell.vue
@@ -11,6 +11,13 @@ import ApartmentColorSvg from '@/components/icons/ApartmentColorSvg.vue'
 import TitlePost from '../title/TitlePost.vue'
 import HouseColorSvg from '@/components/icons/HouseColorSvg.vue'
 import WareHouseSvg from '@/components/icons/WareHouseSvg.vue'
+import BuildingColorSvg from '@/components/icons/BuildingColorSvg.vue'
+import ShopColorSvg from '@/components/icons/ShopColorSvg.vue'
+import BedColorSvg from '@/components/icons/BedColorSvg.vue'
+import HotelColorSvg from '@/components/icons/HotelColorSvg.vue'
+import FincaColorSvg from '@/components/icons/FincaColorSvg.vue'
+import ConsultorioColorSvg from '@/components/icons/ConsultorioColorSvg.vue'
+import TerrenoColorSvg from '@/components/icons/TerrenoColorSvg.vue'
 
 const isRenting = ref<boolean>(false)
 const fisrstTime = ref<boolean>(true)
@@ -24,7 +31,6 @@ const rent = () => {
 
 <template>
   <section class="rent-or-sell">
-
 
     <TitlePost v-if="fisrstTime" title="¿Está buscando vender o arrendar su propiedad?" />
 
@@ -43,7 +49,6 @@ const rent = () => {
       </div>
     </div>
 
-
     <TitlePost v-if="isRenting" title="¿Qué tipo de inmueble es?" />
 
     <div class="rent-options" v-if="isRenting">
@@ -53,24 +58,69 @@ const rent = () => {
           <ApartmentColorSvg />
         </div>
       </div>
+
       <div class="rent-option">
         <p>Casas</p>
         <div class="rent-option-svg">
           <HouseColorSvg />
         </div>
       </div>
+
       <div class="rent-option">
         <p>Bodega</p>
         <div class="rent-option-svg">
           <WareHouseSvg />
         </div>
       </div>
+
       <div class="rent-option">
-        <p>Consultorio</p>
+        <p>Locales</p>
         <div class="rent-option-svg">
-          <OfficeSvg />
+          <ShopColorSvg />
         </div>
       </div>
+
+      <div class="rent-option">
+        <p>Edificio</p>
+        <div class="rent-option-svg">
+          <BuildingColorSvg />
+        </div>
+      </div>
+
+      <div class="rent-option">
+        <p>Habitaciones</p>
+        <div class="rent-option-svg">
+          <BedColorSvg />
+        </div>
+      </div>
+
+      <div class="rent-option">
+        <p>Hoteles</p>
+        <div class="rent-option-svg">
+          <HotelColorSvg />
+        </div>
+      </div>
+
+      <div class="rent-option">
+        <p>Fincas</p>
+        <div class="rent-option-svg">
+          <FincaColorSvg />
+        </div>
+      </div>
+      <div class="rent-option">
+        <p>Consultorios</p>
+        <div class="rent-option-svg">
+          <ConsultorioColorSvg />
+        </div>
+      </div>
+
+      <div class="rent-option">
+        <p>Terrenos y Lotes</p>
+        <div class="rent-option-svg">
+          <TerrenoColorSvg />
+        </div>
+      </div>
+
     </div>
 
   </section>
