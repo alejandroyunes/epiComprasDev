64a4fa4357edc4313415d2372e45ae0a3b464e23
alejandro.cano@imagineapps.co <alejandroyunes@outlook.com>
2024-03-31 19:28:28 -0500

    fixing styles in items svgs

--- src/components/ad-post/real-estate/RealEstate.vue
+++ src/components/ad-post/real-estate/RealEstate.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import './real-estate.scss'
+import '../ad-post.scss'
 import HandWithKeySvg from '../icons/real-estate/rental-or-sell/HandWithKeySvg.vue'
 import ContractAgreeSvg from '../icons/real-estate/rental-or-sell/ContractAgreeSvg.vue'
 import ApartmentColorSvg from '../icons/real-estate/type-of-property/ApartmentColorSvg.vue'
@@ -35,15 +36,15 @@ const rent = () => {
     <GoBack />
 
     <div class="rent-or-sell-options">
-      <div class="rent-or-sell-item" @click="rent">
+      <div class="ad-post-item" @click="rent">
         <p>Arrendar</p>
-        <div class="rent-or-sell-svg">
+        <div class="ad-post-svg">
           <HandWithKeySvg />
         </div>
       </div>
-      <div class="rent-or-sell-item" @click="rent">
+      <div class="ad-post-item" @click="rent">
         <p>Vender</p>
-        <div class="rent-or-sell-svg">
+        <div class="ad-post-svg">
           <ContractAgreeSvg />
         </div>
       </div>
@@ -56,72 +57,72 @@ const rent = () => {
     <GoBack />
 
     <div class="rent-options-wrapper">
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Apartmentos</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <ApartmentColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Casas</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <HouseColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Bodega</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <WareHouseSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Locales</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <ShopColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Edificio</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <BuildingColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Habitaciones</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <BedColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Hoteles</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <HotelColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Fincas</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <FincaColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Consultorios</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <ConsultorioColorSvg />
         </div>
       </div>
 
-      <div class="rent-option">
+      <div class="ad-post-item">
         <p>Terrenos y Lotes</p>
-        <div class="rent-option-svg">
+        <div class="ad-post-svg">
           <TerrenoColorSvg />
         </div>
       </div>
